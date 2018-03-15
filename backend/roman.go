ed60825986ee46eb10f0c9192b5f534aee3854c5
dima <dimabondarenko528@gmail.com>
2018-03-15 17:02:33 +0200

    Rename convert func

--- backend/roman.go
+++ backend/roman.go
@@ -5,13 +5,13 @@ import (
 	"log"
 )
 
-func convert(w http.ResponseWriter, r *http.Request) {
+func Convert(w http.ResponseWriter, r *http.Request) {
 	
 }
 
 
 func main() {
-	http.HandleFunc("/convert", convert)
+	http.HandleFunc("/convert", Convert)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
