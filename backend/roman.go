0b7f09c75bcefc71af22903befd44dbc989b0d1c
dima <dimabondarenko528@gmail.com>
2018-03-18 17:50:19 +0200

    Some fixes

--- backend/roman.go
+++ backend/roman.go
@@ -2,31 +2,32 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 	"log"
 	"net/http"
 	"regexp"
 	"strconv"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/cors"
-	"github.com/go-chi/chi/middleware"
+	"strings"
 )
+
+var table = [][]string{
+	{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
+	{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+	{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+	{"", "M", "MM", "MMM"},
+}
 
 func Convert(w http.ResponseWriter, r *http.Request) {
 	numbers, ok := r.URL.Query()["number"]
-	if !ok || len(numbers) != 1 {
+	if !ok || len(numbers) != 1 || numbers[0] == "" {
 		http.Error(w, "Bad number", 500)
 		return
 	}
-	number := numbers[0]
+	number := strings.ToUpper(numbers[0])
 
-	table := [][]string{
-		{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
-		{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
-		{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
-		{"", "M", "MM", "MMM"},
-	}
-
-	romanRegexp := regexp.MustCompile(`^(M{0,3})(D?C{0,3}|C[DM])(L?X{0,3}|X[LC])(V?I{0,3}|I[VX])$`)
+	re := regexp.MustCompile(`^(M{0,3})(D?C{0,3}|C[DM])(L?X{0,3}|X[LC])(V?I{0,3}|I[VX])$`)
 
 	var result interface{}
 	if arab, err := strconv.Atoi(number); err == nil {
@@ -45,10 +46,10 @@ func Convert(w http.ResponseWriter, r *http.Request) {
 			digit /= 10
 		}
 
-		result = map[string]string {"Res": string(roman)}
-	} else if romanRegexp.MatchString(number) {
+		result = map[string]string{"Res": string(roman)}
+	} else if re.MatchString(number) {
 		// Convert roman to arab.
-		digits := romanRegexp.FindAllStringSubmatch(number, 1)
+		digits := re.FindAllStringSubmatch(number, 1)
 
 		arab := 0
 		for i, digit := 1, 1000; i < len(digits[0]); i, digit = i+1, digit/10 {
@@ -60,7 +61,7 @@ func Convert(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 
-		result = map[string]int {"Res": arab}
+		result = map[string]int{"Res": arab}
 	} else {
 		http.Error(w, "Bad number", 500)
 		return
@@ -79,18 +80,19 @@ func main() {
 	r := chi.NewRouter()
 
 	CORS := cors.New(cors.Options{
-		AllowedOrigins:		[]string{"*"},
-		AllowedMethods:		[]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:		[]string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:		[]string{"Link"},
-		AllowCredentials:	true,
-		MaxAge:				300,
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
 	})
 	r.Use(CORS.Handler)
 	r.Use(middleware.SetHeader("Content-Type", "application/json"))
 
 	r.Get("/convert", Convert)
 
+	log.Printf("Server run on http://localhost:8080")
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
